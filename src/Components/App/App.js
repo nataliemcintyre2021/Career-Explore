77df34b0a92c42898e177d2e3cc70a5e891029ef
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-23 10:23:42 -0700

    Add form component and implement search feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,36 @@
 import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Home from '../Home/Home';
 import SearchForm from '../SearchForm/SearchForm'
 
 const App = () => {
 
+const fetchPositions = (position) => {
+  const url = `https://data.usajobs.gov/api/Search?PositionTitle=${position}`;
+  var host = 'data.usajobs.gov';
+  var userAgent = 'nataliemcintyre2021@gmail.com';
+  var authKey = '/OzvruqvX5Jcf9J84/TH1epCY/n9yJu6q1PIOGw4AMI=';
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      "Host": host,
+      "User-Agent": userAgent,
+      "Authorization-Key": authKey
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+}
 
 
   return (
 
     <>
       <Header />
-  
-      <Home />
+
+      <SearchForm fetchPositions={fetchPositions}/>
 
       <Footer />
     </>
--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,20 +1,50 @@
+import { useState } from 'react';
 import "./SearchForm.css"
 
-const SearchForm = () => {
+
+const SearchForm = ({ fetchPositions }) => {
+  const [positionInput, setPositionInput] = useState("");
+
+  const handleChange = event => {
+    setPositionInput(event.target.value)
+  }
+
+  const submitInput = event => {
+    event.preventDefault();
+    fetchPositions(positionInput)
+    clearInputs()
+  }
+
+  const clearInputs = () => {
+    setPositionInput("");
+  }
+
+
   return (
-    <section class="form-search">
-      <form>
-        <input
-          type='text'
-          placeholder='Ex: Software Engineer'
-          name='jobName'
-        />
-        <button className="job-search">SEARCH</button>
-      </form>
-    </section>
+    <>
+
+    <main className="main-page">
+
+      <section className="search-prompt">
+        <p>Search for any government job title below and learn about what they get to do!</p>
+      </section>
+      <section className="form-search">
+        <form>
+          <input
+            type='text'
+            placeholder='Ex: Software Engineer'
+            name='positionInput'
+            value={positionInput}
+            onChange={event => handleChange(event)}
+          />
+          <button className="job-search" onClick={(event) => submitInput(event)}>Search</button>
+        </form>
+
+      </section>
+
+    </main>
+    </>
   )
 }
-
-
 
 export default SearchForm;
