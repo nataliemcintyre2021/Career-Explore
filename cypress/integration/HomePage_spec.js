2555e3c3f729cbc093f4573b094ab456455c1891
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-27 10:22:21 -0700

    Write tests for search pompts

--- cypress/integration/HomePage_spec.js
+++ cypress/integration/HomePage_spec.js
@@ -9,11 +9,22 @@ describe("User Career Search Page Flows", () => {
       cy.get('.header').find('a').find('.title').find('h1').contains("Career Explore")
     })
 
-    it("Should have a menu with Favorites and Job Resources on load", () => {
-      cy.get('.header').find('.menu').find('.favs').find('.favorites-page').contains('Favorites')
-      cy.get('.header').find('.menu').find('.favs').should('have.attr', 'href').and('include', 'favorites')
-      cy.get('.header').find('.menu').find('.jobs').find('.resources-page').contains('Job Resources')
-      cy.get('.header').find('.menu').find('.jobs').should('have.attr', 'href').and('include', 'job-resources')
+    it("Should have a menu with Home and Favorites on load", () => {
+      cy.get('.header').find('.menu').find('.home').contains('Home')
+      cy.get('.header').find('.menu').find('.home').should('have.attr', 'href').and('include', '/')
+      cy.get('.header').find('.menu').find('.favs').contains('Favorites')
+      cy.get('.header').find('.menu').find('.favs').should('have.attr', 'href').and('include', '/favorites')
+    })
+
+    it("Should display prompts before search bar area", () => {
+      cy.get('.form-search').find('.the-prompts').find('.arrow-container').find('.question').contains('Do you have interest in a US government role?')
+      cy.get('.form-search').find('.the-prompts').find('.arrow-container2').find('.question').contains('Put on any job “hat” for a moment and imagine yourself in that position!')
+    })
+
+    it("Should display search prompt and search bar with button", () => {
+      cy.get('.form-search').find('.the-search').find('.prompt').contains('Search for any government job title and learn about what they get to do!')
+      cy.get('.form-search').find('.the-search').find('input')
+      cy.get('.form-search').find('.the-search').find('.job-search').contains('Search')
     })
   })
 })
