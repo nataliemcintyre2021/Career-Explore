a04dc71d1a990890923c645b079b36ad8bfd6835
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-26 17:27:06 -0700

    Create HomeSpec testing file

---
+++ cypress/integration/HomePage_spec.js
@@ -0,0 +1,8 @@
+describe("User Career Search Page Flows", () => {
+
+  describe("Main Page Render", () => {
+    it("Should confirm that true is true", () => {
+      expect(true).to.equal(true)
+    })
+  })
+})
