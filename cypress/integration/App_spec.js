ce0574ecd23590706f1da0f0d0a6356d5f0916a6
nataliemcintyre2021 <nataliemcintyre2021@gmail.com>
2021-10-07 10:58:58 -0700

    Update test for details page

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -41,7 +41,14 @@ describe("User Career Explore Page Flows", () => {
   describe("Details and Favorites Page Render", () => {
 
     it("Should show job duty details of specific job posting", () => {
-      cy.visit('http://localhost:3000/Software%20Engineer/599068800')
+      cy.intercept("GET", 'https://data.usajobs.gov/api/Search?PositionTitle=**', { fixture: 'software-engineer-search-data.json', status: 200})
+      cy.get('input[type="text"]')
+        .type('Software Engineer')
+        .should('have.value', 'Software Engineer')
+      cy.get('button').click()
+      cy.get('.search-results-title').contains("Software Engineer")
+      cy.get('.job-container').find('.card').find('p').contains('PROF OF SOFTWARE ENGINEERING MANAGEMENT (DEV/SEC/OPS)')
+      cy.get('.job-container').find('.card').click()
       cy.get('.the-details-card').find('.duties-details').find('.duty').contains("This position develops training materials and provides training to the DoD acquisition workforce in the areas associated with engineering management, with emphasis on modern software development methodologies. The holder of the position will also conduct research and analysis of defense acquisition policy issues. Serves as a Professor of Software Engineering Management with extensive experience and knowledge in modern software development methods (DevSecOps, Agile) and software product areas (Weapons Systems, AI, Cyber, Defense Business Systems, etc), software assurance and provenance for large and complex military systems. Performs as a \"thinking partner\" and consultant with acquisition customers and students, continuously improving product and service delivery to provide the right knowledge and skills at the point of need through mission assistance, workshops, classroom offerings and on-line resources. Creates content and coordinates with learning asset and technology subject matter experts within the enterprise for development of curriculum, to modify assets and enhance delivery methods as needed. Develops and conducts lectures, cases, simulations, workshops, and field trips. Leads and facilitates student discussion, problem solving, and presentations. Prepares course materials (student and instructor) according to current instructional systems design technology and develops course schedules. Develops and analyzes student assessments, and provides student counseling to ensure the highest degree of learning. Develops or contributes to development of online assets (e.g. courses, podcasts, videos) that customers can leverage in the absence of direct access to DAU faculty. Participates in professional research to ensure curriculum currency and to enhance the skill set and processes in your field of expertise. Performs other duties as assigned.")
     })
 
